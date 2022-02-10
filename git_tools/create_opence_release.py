76fa8b22b81b3f960f8d3e9043f42a9e6ebce8ee
Nishidha Panpaliya <npanpa23@in.ibm.com>
2022-02-10 15:25:08 +0000

    Automate tagging for TF serving too

--- git_tools/create_opence_release.py
+++ git_tools/create_opence_release.py
@@ -73,94 +73,95 @@ def _main(arg_strings=None): # pylint: disable=too-many-locals, too-many-stateme
         config_file = os.path.abspath(args.conda_build_configs)
 
     primary_repo_path = "./"
+    env_files = ["opence-env.yaml", "tensorflow-serving-env.yaml"]
+    for env_file in env_files:
+        open_ce_env_file = os.path.abspath(os.path.join(primary_repo_path, "envs", env_file))
+        if not _has_git_tag_changed(primary_repo_path, args.branch, open_ce_env_file):
+            print("--->The opence-env git_tag has not changed.")
+            print("--->No release is needed.")
+            return
+        print("--->The opence-env git_tag has changed!")
+        current_tag = _get_git_tag_from_env_file(open_ce_env_file)
+        previous_tag = _get_previous_git_tag_from_env_file(primary_repo_path, args.branch, open_ce_env_file)
+        version = _git_tag_to_version(current_tag)
+        release_number = ".".join(version.split(".")[:-1])
+        bug_fix = version.split(".")[-1]
+        branch_name = "open-ce-r{}".format(release_number)
+        version_msg = "Open-CE Version {}".format(version)
+        release_name = "v{}".format(version)
 
-    open_ce_env_file = os.path.abspath(os.path.join(primary_repo_path, "envs", "opence-env.yaml"))
-    if not _has_git_tag_changed(primary_repo_path, args.branch, open_ce_env_file):
-        print("--->The opence-env git_tag has not changed.")
-        print("--->No release is needed.")
-        return
-    print("--->The opence-env git_tag has changed!")
-    current_tag = _get_git_tag_from_env_file(open_ce_env_file)
-    previous_tag = _get_previous_git_tag_from_env_file(primary_repo_path, args.branch, open_ce_env_file)
-    version = _git_tag_to_version(current_tag)
-    release_number = ".".join(version.split(".")[:-1])
-    bug_fix = version.split(".")[-1]
-    branch_name = "open-ce-r{}".format(release_number)
-    version_msg = "Open-CE Version {}".format(version)
-    release_name = "v{}".format(version)
-
-    env_file_contents = env_config.load_env_config_files([open_ce_env_file], utils.ALL_VARIANTS(), ignore_urls=True)
-    for env_file_content in env_file_contents:
-        env_file_tag = env_file_content.get(env_config.Key.git_tag_for_env.name, None)
-        if env_file_tag != current_tag:
-            message = "Incorrect {} '{}' found in the following env_file:\n{}".format(env_config.Key.git_tag_for_env.name,
+        env_file_contents = env_config.load_env_config_files([open_ce_env_file], utils.ALL_VARIANTS(), ignore_urls=True)
+        for env_file_content in env_file_contents:
+            env_file_tag = env_file_content.get(env_config.Key.git_tag_for_env.name, None)
+            if env_file_tag != current_tag:
+                message = "Incorrect {} '{}' found in the following env_file:\n{}".format(env_config.Key.git_tag_for_env.name,
                                                                                       env_file_tag,
                                                                                       env_file_content)
-            raise Exception(message)
+                raise Exception(message)
 
-    if not git_utils.branch_exists(primary_repo_path, branch_name):
-        print("--->Creating {} branch in {}".format(branch_name, args.primary_repo))
-        git_utils.create_branch(primary_repo_path, branch_name)
-    else:
-        print("--->Branch {} already exists in {}. Not creating it.".format(current_tag, args.primary_repo))
+        if not git_utils.branch_exists(primary_repo_path, branch_name):
+            print("--->Creating {} branch in {}".format(branch_name, args.primary_repo))
+            git_utils.create_branch(primary_repo_path, branch_name)
+        else:
+            print("--->Branch {} already exists in {}. Not creating it.".format(current_tag, args.primary_repo))
 
-    print("--->Tag Primary Branch")
-    git_utils.create_tag(primary_repo_path, current_tag, version_msg)
+        print("--->Tag Primary Branch")
+        git_utils.create_tag(primary_repo_path, current_tag, version_msg)
 
-    if args.not_dry_run:
-        print("--->Pushing branch.")
-        git_utils.push_branch(primary_repo_path, branch_name)
-        print("--->Pushing tag.")
-        git_utils.push_branch(primary_repo_path, current_tag)
-    else:
-        print("--->Skipping pushing branch and tag for dry run.")
+        if args.not_dry_run:
+            print("--->Pushing branch.")
+            git_utils.push_branch(primary_repo_path, branch_name)
+            print("--->Pushing tag.")
+            git_utils.push_branch(primary_repo_path, current_tag)
+        else:
+            print("--->Skipping pushing branch and tag for dry run.")
 
-    repos = _get_all_feedstocks(env_files=env_file_contents,
-                                github_org=args.github_org,
-                                pat=args.pat,
-                                skipped_repos=[args.primary_repo, ".github"] + inputs.parse_arg_list(args.skipped_repos))
+        repos = _get_all_feedstocks(env_files=env_file_contents,
+                                    github_org=args.github_org,
+                                    pat=args.pat,
+                                    skipped_repos=[args.primary_repo, ".github"] + inputs.parse_arg_list(args.skipped_repos))
 
-    repos.sort(key=lambda repo: repo["name"])
+        repos.sort(key=lambda repo: repo["name"])
 
-    tag_all_repos.clone_repos(repos=repos,
-                              branch=None,
-                              repo_dir=args.repo_dir,
-                              prev_tag=previous_tag)
-    tag_all_repos.tag_repos(repos=repos,
-                            tag=current_tag,
-                            tag_msg=version_msg,
-                            repo_dir=args.repo_dir)
-    if args.not_dry_run:
-        tag_all_repos.push_repos(repos=repos,
+        tag_all_repos.clone_repos(repos=repos,
+                                  branch=None,
+                                  repo_dir=args.repo_dir,
+                                  prev_tag=previous_tag)
+        tag_all_repos.tag_repos(repos=repos,
                                 tag=current_tag,
-                                repo_dir=args.repo_dir,
-                                continue_query=False)
-    else:
-        print("--->Skipping pushing feedstocks for dry run.")
+                                tag_msg=version_msg,
+                                repo_dir=args.repo_dir)
+        if args.not_dry_run:
+            tag_all_repos.push_repos(repos=repos,
+                                     tag=current_tag,
+                                     repo_dir=args.repo_dir,
+                                     continue_query=False)
+        else:
+            print("--->Skipping pushing feedstocks for dry run.")
 
-    print("--->Generating Release Notes.")
-    release_notes = _create_release_notes(repos,
-                                          version,
-                                          release_number,
-                                          bug_fix,
-                                          current_tag,
-                                          previous_tag,
-                                          utils.ALL_VARIANTS(),
-                                          config_file,
-                                          repo_dir=args.repo_dir,)
-    print(release_notes)
+        print("--->Generating Release Notes.")
+        release_notes = _create_release_notes(repos,
+                                              version,
+                                              release_number,
+                                              bug_fix,
+                                              current_tag,
+                                              previous_tag,
+                                              utils.ALL_VARIANTS(),
+                                              config_file,
+                                              repo_dir=args.repo_dir,)
+        print(release_notes)
 
-    if args.not_dry_run:
-        print("--->Creating Draft Release.")
-        git_utils.create_release(args.github_org,
-                                 args.primary_repo,
-                                 args.pat,
-                                 current_tag,
-                                 release_name,
-                                 release_notes,
-                                 True)
-    else:
-        print("--->Skipping release creation for dry run.")
+        if args.not_dry_run:
+            print("--->Creating Draft Release.")
+            git_utils.create_release(args.github_org,
+                                     args.primary_repo,
+                                     args.pat,
+                                     current_tag,
+                                     release_name,
+                                     release_notes,
+                                     True)
+        else:
+            print("--->Skipping release creation for dry run.")
 
 def _get_git_tag_from_env_file(env_file):
     '''
